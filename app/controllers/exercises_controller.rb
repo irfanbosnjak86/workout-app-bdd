0fbf6529370ff9809988c15288cc53f901a86c42
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2017-07-28 00:15:47 +0000

    Adds invalid scenario for exercise.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,19 +8,19 @@ class ExercisesController < ApplicationController
   end
   
   def create
-    @exercise = current_user.exercises.new(exercise_params)
-    
+   @exercise = current_user.exercises.new(exercise_params)
+
     if @exercise.save
       flash[:notice] = "Exercise has been created"
-      redirect_to [current_user, @exercise]
+      redirect_to user_exercise_path(current_user, @exercise)
     else
-      flash[:alert] = "Exercise has not been created"
-      render 'new'
+      flash.now[:notice] = "Exercise has not been created"
+      render :new
     end
   end
   
   def show  
-    @exercise = current_user.exercises.find(params[:id])
+    @exercise = Exercise.find(params[:id])
   end
   
   private 
--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.feature "Creating Exercise" do
   before do
-    @test_user = User.create(email: "test_user@test.com", password: "12345678")
+    @test_user = User.create!(email: "test_user@test.com", password: "12345678")
     login_as(@test_user)
-  end
-  
-  scenario "with valid inputs" do
+    
     visit "/"
     
     click_link "My Lounge"
     click_link "New workout"
+  end
+  
+  scenario "with valid inputs" do
+ 
     
     expect(page).to have_link("Back")
     
@@ -26,5 +28,19 @@ RSpec.feature "Creating Exercise" do
     
     expect(current_path).to eq(user_exercise_path(@test_user, exercise))
     expect(exercise.user_id).to eq(@test_user.id)
+  end
+  
+  scenario "with invalid inputs" do
+    expect(page).to have_link("Back")
+    
+    fill_in "Duration", with: ""
+    fill_in "Workout Details",  with: ""
+    fill_in "Activity Date",  with: ""
+    click_button "Create Exercise"
+    
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content("Duration in min is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
   end
 end
