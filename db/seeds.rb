c9ae7b7cd9f857e290db50e591ba2ad15801dddc
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2017-07-29 13:14:39 +0000

    Adds users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!(email: "some@hotmail.com", first_name: "some", last_name: "user", password: "12345678")
+User.create!(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+User.create!(first_name: "Sarah", last_name: "Poon", email: "sarah@example.com", password: "password")
+User.create!(first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
+User.create!(first_name: "Ben", last_name: "Gillespie", email: "ben@example.com", password: "password")
+User.create!(first_name: "Tony", last_name: "Hall", email: "tony@example.com", password: "password")
+User.create!(first_name: "Brendon", last_name: "Terry", email: "brendon@example.com", password: "password")
+User.create!(first_name: "Heidi", last_name: "Tompson", email: "heidi@example.com", password: "password")
+User.create!(first_name: "Rachel", last_name: "Andreson", email: "rachel@example.com", password: "password")
+User.create!(first_name: "Sussy", last_name: "Birns", email: "sussy@example.com", password: "password")
+User.create!(first_name: "kate", last_name: "Richards", email: "kate@example.com", password: "password")
